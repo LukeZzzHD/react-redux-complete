873084c41ede814494aa6ef6d151d40d4dbe4e83
Lukas Schwab <lukas.schwab@tamedia.ch>
2019-03-01 15:29:12 +0100

    Started poketimes setup

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos pariatur voluptatibus tempore ratione quo consequatur ullam dicta quisquam ab minus, et labore accusamus! Nostrum nisi magnam dolor delectus consequatur aliquid!</p>
+        </div>
+    );
+}
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos pariatur voluptatibus tempore ratione quo consequatur ullam dicta quisquam ab minus, et labore accusamus! Nostrum nisi magnam dolor delectus consequatur aliquid!</p>
+        </div>
+    );
+}
+
+export default Home
